66d3a4a3ec679e99b1219619243d1436c924f278
nandosannn <silvafernandosan@gmail.com>
2024-10-25 11:08:56 -0300

    Update_tarefa3

--- tarefa3/Main.java
+++ tarefa3/Main.java
@@ -1,30 +1,42 @@
-import src.model.Account;
+import src.model.Ball;
 
 public class Main {
-    public static void main(String[] args) {
-        // Test constructor and toString()
-      Account a1 = new Account("A101", "Tan Ah Teck", 88);
-      System.out.println(a1);  // toString();
-      Account a2 = new Account("A102", "Kumar"); // default balance
-      System.out.println(a2);
+  public static void main(String[] args) {
+    // Test constructor and toString()
+      Ball ball = new Ball(1.1f, 2.2f, 10, 3.3f, 4.4f);
+      System.out.println(ball);  // toString()
 
-      // Test Getters
-      System.out.println("ID: " + a1.getId());
-      System.out.println("Name: " + a1.getName());
-      System.out.println("Balance: " + a1.getBalance());
+      // Test Setters and Getters
+      ball.setX(80.0f);
+      ball.setY(35.0f);
+      ball.setRadius(5);
+      ball.setXDelta(4.0f);
+      ball.setYDelta(6.0f);
+      System.out.println(ball);  // toString()
+      System.out.println("x is: " + ball.getX());
+      System.out.println("y is: " + ball.getY());
+      System.out.println("radius is: " + ball.getRadius());
+      System.out.println("xDelta is: " + ball.getXDelta());
+      System.out.println("yDelta is: " + ball.getYDelta());
 
-      // Test credit() and debit()
-      a1.credit(100);
-      System.out.println(a1);
-      a1.debit(50);
-      System.out.println(a1);
-      a1.debit(500);  // debit() error
-      System.out.println(a1);
-
-      // Test transfer()
-      a1.transferTo(a2, 100);  // toString()
-      System.out.println(a1);
-      System.out.println(a2);
-    
-    }
+      // Bounce the ball within the boundary
+      float xMin = 0.0f;
+      float xMax = 100.0f;
+      float yMin = 0.0f;
+      float yMax = 50.0f;
+      for (int i = 0; i < 15; i++) {
+         ball.move();
+         System.out.println(ball);
+         float xNew = ball.getX();
+         float yNew = ball.getY();
+         int radius = ball.getRadius();
+         // Check boundary value to bounce back
+         if ((xNew + radius) > xMax || (xNew - radius) < xMin) {
+            ball.reflectHorizontal();
+         }
+         if ((yNew + radius) > yMax || (yNew - radius) < yMin) {
+            ball.reflectVertical();
+         }
+      }
+  }
 }
